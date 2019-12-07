561b21404adc5ab59b294a0bd290d9405c230e10
cotton <chinoknct@gmail.com>
2019-12-07 18:13:02 +0900

    表を設置

---
+++ client/pages/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="container">
+    <el-row>
+      <el-col :span="10">
+        <el-input placeholder="Please input" v-model="input"></el-input>
+      </el-col>
+      <el-col :span="10">
+        <el-input placeholder="Please input" v-model="input"></el-input>
+      </el-col>
+      <el-table
+          :data="tableData"
+          border
+          style="width: 90%">
+        <el-table-column
+          prop="date"
+          label="Date"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="Name"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="Address">
+        </el-table-column>
+      </el-table>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data() {
+    return {
+      input: '',
+      tableData: [{
+        date: '2016-05-03',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-04',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }]
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+
+.el-col {
+  border-radius: 4px;
+}
+</style>

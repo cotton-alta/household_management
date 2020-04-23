7a4967a5c86d2882d60fad7a90060692b6c8cd54
cotton <chinoknct@gmail.com>
2020-04-23 16:23:58 +0900

    [add]データベース接続用設定の追加
    データベースに接続するために使用する関数群を追加しました。

--- api/controllers/tableController.go
+++ api/controllers/tableController.go
@@ -1,23 +1,57 @@
 package controllers
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
-	// "database/sql"
-	// "log"
-	// "time"
-	// "gopkg.in/gorp.v1"
-	// _ "github.com/mattn/go-sqlite3"
+	"gopkg.in/gorp.v2"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/labstack/echo"
 )
 
+type HouseHold struct {
+	Id      int64  `db:"post_id"`
+	Created int64
+	Title   string `db:",size:50"`               
+	Body    string `db:"article_body,size:1024"` 
+}
+
 func GetTable() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		// dbmap := initDb()
-		// defer dbmap.Db.Close()
+		dbmap := initDb()
+		defer dbmap.Db.Close()
 
-		// err := dbmap.TruncateTables()
-		// checkErr(err, "TruncateTables failed")
+		err := dbmap.TruncateTables()
+		checkErr(err, "TruncateTables failed")
+
+		var management  HouseHold
+		//SQLのクエリを直接記入（今回はselect文）
+		err = dbmap.SelectOne(&management, "select * from household where id=?", 1)
 
 		return c.String(http.StatusOK, "get table!")
+	}
+}
+
+//dbに関する設定、今後modelsに移行予定
+func initDb() *gorp.DbMap {
+	db, err := sql.Open("mysql", "/example")
+	checkErr(err, "sql.Open failed")
+
+	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{"InnoDB", "UTF-8"}}
+
+	err = dbmap.CreateTablesIfNotExists()
+	checkErr(err, "Create tables failed")
+	
+	dbmap.AddTableWithName(HouseHold{}, "household").SetKeys(true, "Id")
+	fmt.Printf("%v", management)
+
+	return dbmap
+}
+
+//エラーハンドリング用関数
+func checkErr(err error, msg string) {
+	if err != nil {
+			log.Fatalln(msg, err)
 	}
 }
--- api/go.mod
+++ api/go.mod
@@ -10,4 +10,5 @@ require (
 	github.com/valyala/fasttemplate v1.1.0 // indirect
 	golang.org/x/crypto v0.0.0-20200420201142-3c4aac89819a // indirect
 	gopkg.in/gorp.v1 v1.7.2
+	gopkg.in/gorp.v2 v2.2.0
 )
